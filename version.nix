00449eac7172146bbad4047c532c9003df13f7e9
abclop99 <13627991+abclop99@users.noreply.github.com>
2024-08-25 12:36:05 +0000

    Update to version 1.35.5

--- version.nix
+++ version.nix
@@ -1,5 +1,5 @@
 {
-  version="1.35.4";
-  id="182296297";
-  hash="sha256-uIeLFAd/TMB3YCGr+kNT5+/sGnZlR5Rw9aevLh3nVjw=";
+  version="1.35.5";
+  id="188172275";
+  hash="sha256-xJHffhhuVDlwUyFNgxbXnKGOpUWlTtZzoMs7DBNuxrg=";
 }
