27f080e808b4eecd056be431b4a0ff007e172d2b
abclop99 <13627991+abclop99@users.noreply.github.com>
2025-03-16 05:20:20 +0000

    Update to version 1.42.1

--- version.nix
+++ version.nix
@@ -1,5 +1,5 @@
 {
-  version="1.38.1";
-  id="232635197";
-  hash="sha256-8taYRT23dUKShEYlnNM8QkNkEGNH5JATv5UGu4H4r5k=";
+  version="1.42.1";
+  id="237882046";
+  hash="sha256-n1napENrKgH5GcxGb9rWo7HdK7b8NOAXKE0BomH0uMY=";
 }
